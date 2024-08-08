af714c8255655aac97806096f4dd053a9236792c
fcursino <fabriciocursino09@gmail.com>
2024-08-08 20:54:46 -0300

    adding hooks test pages in app router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import Useeffect from './pages/useEffect'
+import Usetransition from './pages/useTransition'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/useeffect' element={<Useeffect/>} />
+        <Route path='/usetransition' element={<Usetransition />} />
       </Routes>
     </Router>
   )
