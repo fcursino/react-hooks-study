cdc963016ffebd38477abc4152a163b83ef15f68
fcursino <fabriciocursino09@gmail.com>
2024-08-09 21:02:27 -0300

    adding use state and use reducer hooks' pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Useeffect from './pages/useEffect'
 import Usetransition from './pages/useTransition'
+import Usereducer from './pages/useReducer'
+import Usestate from './pages/useState'
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/useeffect' element={<Useeffect/>} />
         <Route path='/usetransition' element={<Usetransition />} />
+        <Route path='/usereducer' element={<Usereducer />} />
+        <Route path='/usestate' element={<Usestate />} />
       </Routes>
     </Router>
   )
